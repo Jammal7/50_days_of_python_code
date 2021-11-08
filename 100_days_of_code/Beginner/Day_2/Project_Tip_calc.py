f9ca81811e30bc80e937e65954c7686419ac88e0
Adeyemi Jammal <oabass7@gmail.com>
2021-11-08 13:04:49 +0100

    mistake

--- 100_days_of_code/Beginner/Day_2/Project_Tip_calc.py
+++ 100_days_of_code/Beginner/Day_2/Project_Tip_calc.py
@@ -3,9 +3,6 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
-from typing import final
-
-
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? "))
 tip = int(input("How much tip would you like to give? "))
