e89d0d8b9c2840d9bbc6ff46c5a808a28945b7ee
Adeyemi Jammal <oabass7@gmail.com>
2021-11-09 17:07:04 +0100

    VWX

--- 100_days_of_code/Beginner/Day_3/conditional_operators.py
+++ 100_days_of_code/Beginner/Day_3/conditional_operators.py
@@ -1,14 +1,23 @@
 print("Welcome to the rollercoster!")
 height = int(input("Input your height in cm: "))
-
+bill = float(0)
 if height >= 120:
     print("You can ride the rollercoster!")
     age = int(input("Input your age: "))
     if age < 12:
-        print("Please pay £5.0")
+        bill = 5
+        print("Child ticket is £5.0")
     elif age >= 18:
-        print("Please pay £15.0")
+        bill = 15
+        print("Adult tickets are £15.0")
     else:
-        print("Please pay £7.50")
+        bill = 7.5
+        print("Teenage tickets are £7.50")
+    
+    photos = input("Do you want a photo taken? Y or N: ")
+    if photos == "Y":
+        # Add £3 to their bill
+        bill += 3
+    print(f"Your final bill is £{bill}")
 else:
-    print("sorry, you have tp grow taller before you can ride. ")
+    print("sorry, you have tp grow taller before you can ride. ") 
