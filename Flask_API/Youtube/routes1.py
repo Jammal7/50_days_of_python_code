a1b6d72951b9678fc6dc8b0131333b928fc35aaf
Adeyemi Jammal <oabass7@gmail.com>
2022-06-09 06:16:01 +0100

    Flask_api

--- Flask_API/Youtube/routes1.py
+++ Flask_API/Youtube/routes1.py
@@ -6,17 +6,17 @@ from flask import Blueprint, render_template, current_app, request, redirect
 
 main=Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
-def index():
+@main.route('/<name>', methods=['GET', 'POST'])
+def index(name):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
     channel_url = 'https://www.googleapis.com/youtube/v3/channels'
 
     all_data = []
 
-    if request.method == 'POST':
+    if request.method == 'GET':
         id_params = {
             'key': current_app.config['YOUTUBE_API_KEY'],
-            'q' : request.form.get('query'),
+            'q' : name,
             'part': 'snippet',
             'type': 'channel'
         }
@@ -35,12 +35,14 @@ def index():
         results = r.json()['items'] 
         for result in results:
             channel_data = {
+                'id' : result['id'],
+                'url' : f'https://www.youtube.com/channel/{result["id"]}',
                 'Channel_name' : result['snippet']['title'],
-                'Subscribers' : result['statistics']['subscriberCount'],
-                'Views' : result['statistics']['viewCount'],
-                'Total_videos' : result['statistics']['videoCount'],
-                'playlist_id' : result['contentDetails']['relatedPlaylists']['uploads']
+                'thumbnail':result['snippet']['thumbnails']['high']['url'],
+                'Channel_stats' : result['statistics'],
             }
+            print(result)
             all_data.append(channel_data)
 
-    return render_template('index1.html', videos=all_data)
+    # return render_template('index1.html', videos=all_data)
+    return dict(channels=all_data)
